9389842841b4d8436b61199f24633b093d14fbfd
Vikash Chandola <vikashchandola26@gmail.com>
2023-08-05 13:00:10 +0530

    Split vector add example to kernel and non kernel part
    Split vectorAdd example in two parts. vectorAdd_main.cpp contains only
    C++ part. vectorAdd_cuda.cpp part contains only kernel part. This allows
    C++ code part to be compiled without need of nvcc compiler.
    
    Signed-off-by: Vikash Chandola <vikashchandola26@gmail.com>

---
+++ 01_vector_addition/baseline/cuda_kernel.hpp
@@ -0,0 +1,7 @@
+#ifndef __CUDA_KERNEL__
+#define __CUDA_KERNEL__
+namespace cuda
+{
+std::vector<int> vectorAdd(const std::vector<int> &,const  std::vector<int> &);
+}
+#endif
---
+++ 01_vector_addition/baseline/vectorAdd_cuda.cu
@@ -0,0 +1,70 @@
+// This program computes the sum of two vectors of length N
+// By: Nick from CoffeeBeforeArch
+
+#include <cassert>
+#include <vector>
+
+namespace cuda
+{
+namespace __impl
+{
+// CUDA kernel for vector addition
+// __global__ means this is called from the CPU, and runs on the GPU
+__global__ void vectorAdd(const int *__restrict a, const int *__restrict b,
+                          int *__restrict c, int N) {
+  // Calculate global thread ID
+  int tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+  // Boundary check
+  if (tid < N) c[tid] = a[tid] + b[tid];
+}
+} // __impl
+
+std::vector<int> vectorAdd(const std::vector<int> &input_1,
+                           const std::vector<int> &input_2){
+  //validate input is coorectly sized.
+  //All input vectors must be of same size
+  assert(input_1.size() == input_2.size());
+  // Allocate memory on the device
+  size_t input_size = input_1.size();
+  std::vector<int> output;
+  output.reserve(input_size);
+  int *d_input_1, *d_input_2, *d_output;
+  size_t bytes = sizeof(int) * input_size;
+  cudaMalloc(&d_input_1, bytes);
+  cudaMalloc(&d_input_2, bytes);
+  cudaMalloc(&d_output, bytes);
+
+  // Copy data from the host to the device (CPU -> GPU)
+  cudaMemcpy(d_input_1, input_1.data(), bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_input_2, input_2.data(), bytes, cudaMemcpyHostToDevice);
+
+  // Threads per CTA (1024)
+  int NUM_THREADS = 1 << 10;
+
+  // CTAs per Grid
+  // We need to launch at LEAST as many threads as we have elements
+  // This equation pads an extra CTA to the grid if N cannot evenly be divided
+  // by NUM_THREADS (e.g. N = 1025, NUM_THREADS = 1024)
+  int NUM_BLOCKS = (input_size + NUM_THREADS - 1) / NUM_THREADS;
+
+  // Launch the kernel on the GPU
+  // Kernel calls are asynchronous (the CPU program continues execution after
+  // call, but no necessarily before the kernel finishes)
+  __impl::vectorAdd<<<NUM_BLOCKS, NUM_THREADS>>>(d_input_1, d_input_2, d_output, input_size);
+
+  // Copy sum vector from device to host
+  // cudaMemcpy is a synchronous operation, and waits for the prior kernel
+  // launch to complete (both go to the default stream in this case).
+  // Therefore, this cudaMemcpy acts as both a memcpy and synchronization
+  // barrier.
+  cudaMemcpy(output.data(), d_output, bytes, cudaMemcpyDeviceToHost);
+
+  // Free memory on device
+  cudaFree(d_input_1);
+  cudaFree(d_input_2);
+  cudaFree(d_output);
+  return output;
+}
+
+} //cuda
